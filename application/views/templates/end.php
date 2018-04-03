b3dc81adf5f30d4dac99fd3e889b3451489e4d89
Deejay <deejay.castillo.a100646@mcast.edu.mt>
2018-04-03 15:00:04 +0200

    css,fonts, and data files set in root folder no in application. Created a Fixed Footer. To change to stickey later on.

--- application/views/templates/end.php
+++ application/views/templates/end.php
@@ -1,2 +1,8 @@
+    <footer class="navbar fixed-bottom navbar-dark bg-dark">
+        <i class="fab fa-facebook fa-2x"></i>
+        <i class="fab fa-instagram fa-2x"></i>
+    </footer>
+
+
     </body>
 </html>
