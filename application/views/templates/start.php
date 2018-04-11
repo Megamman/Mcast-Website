c0a50e12872476747d837e7b49de593d5cf5ad4b
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-11 16:57:34 +0200

    edited css

--- application/views/templates/start.php
+++ application/views/templates/start.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>MCAST</title>
 
-        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="<?=base_url('css/font-awesome.css')?>">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="<?=base_url('css/style.css')?>">
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <nav class="navbar navbar-dark bg-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button  class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </nav>
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,3 +1,8 @@
 <!-- PUT HTML ETC FOR HOME SCREEN-->
+<div class="container-fluid">
+    <div class="background">
+        <img src="<?=base_url('images/landscape.jpg')?>" alt="" style="width: 100%;">
+    </div>
 
-<img src="<?=base_url('images/landscape.jpg')?>" alt="" style="width: 100%;">
+
+</div>
