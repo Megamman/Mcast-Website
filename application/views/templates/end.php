76356ff030732d185b2b6bc6d57fbcfdf14e9405
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-11 13:02:35 +0200

    base java

--- application/views/templates/end.php
+++ application/views/templates/end.php
@@ -4,7 +4,7 @@
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?=base_url('js/bootstrap.min.js')?>"></script>
 
     </body>
 </html>
