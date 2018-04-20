da2a7ba4c926a54ed4aa4eb79b081dccd836ad78
Deejay <deejay.castillo.a100646@mcast.edu.mt>
2018-04-20 21:08:47 +0200

    trying to fix masonry grid

--- application/views/portofolio.php
+++ application/views/portofolio.php
@@ -1,6 +1,12 @@
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Our Work</h1>
+        <li></li>
+    </div>
+</div>
 
-<div class="container">
-    <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
+<div class="p-5">
+    <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 100 }'>
       <div class="grid-item">post1</div>
       <div class="grid-item grid-item--width2">post2</div>
       <div class="grid-item">post3</div>
